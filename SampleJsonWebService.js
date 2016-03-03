b7d7f8f1ac33f71b4436bf42e6d50bafe307eed1
michaelringholm <chssdm@NB-CHZU-0265.int.audatex.com>
2016-03-03 16:56:43 +0100

    Now handling POST data

--- SampleJsonWebService.js
+++ SampleJsonWebService.js
@@ -1,12 +1,12 @@
 var http = require('http');
 var fs = require('fs');
 
-function saveFile() {
+function saveFile(customData) {
 	var fs = require("fs");
 
 	console.log("Going to write into existing file");
 	var updateTime = new Date();
-	fs.writeFile('save.dat', '{ "map" : "general", "position": "10,25", "updateTime" : "' + updateTime + '" }',  function(err) {
+	fs.writeFile('save.dat', '{ "map" : "general", "position": "10,25", "updateTime" : "' + updateTime + '", "customData" : "' + customData + '" }',  function(err) {
 	if (err) {
 		return console.error(err);
 	}
@@ -22,10 +22,27 @@ http.createServer(function (request, response) {
 	response.write('{"author": "Michael Sundgaard", "company" : "Opus Magus"}');
 	response.end();
   }
-  if(request.url == "/save") {	
-		saveFile();
-		response.write('{ "status": "success"}');
-		response.end();
+  if(request.url == "/save") {
+		if (request.method == 'POST') {
+			var fullBody = '';
+    
+			request.on('data', function(chunk) {
+			  // append the current chunk of data to the fullBody variable
+			  fullBody += chunk.toString();
+			});
+			
+			request.on('end', function() {			
+				// request ended -> do something with the data
+				saveFile(fullBody);
+				response.write('{ "status": "success"}');
+				response.end();
+			});
+		}
+		else {
+			saveFile();
+			response.write('{ "status": "success"}');
+			response.end();
+		}
   }
   else {
 	fs.readFile('save.dat', (err, data) => {		
@@ -37,3 +54,32 @@ http.createServer(function (request, response) {
 }).listen(1337, "127.0.0.1");
 
 console.log('Server running at http://127.0.0.1:1337/');
+
+/*
+if (req.method == 'POST') {
+    console.log("[200] " + req.method + " to " + req.url);
+    var fullBody = '';
+    
+    req.on('data', function(chunk) {
+      // append the current chunk of data to the fullBody variable
+      fullBody += chunk.toString();
+    });
+    
+    req.on('end', function() {
+    
+      // request ended -> do something with the data
+      res.writeHead(200, "OK", {'Content-Type': 'text/html'});
+      
+      // parse the received body data
+      var decodedBody = querystring.parse(fullBody);
+
+      // output the decoded data to the HTTP response          
+      res.write('<html><head><title>Post data</title></head><body><pre>');
+      res.write(utils.inspect(decodedBody));
+      res.write('</pre></body></html>');
+      
+      res.end();
+    });
+    
+  }
+  */
