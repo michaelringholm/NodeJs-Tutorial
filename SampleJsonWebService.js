62cb82fe451c4334fdbb8b5b8784ea7d3d0d4180
michaelringholm <chssdm@NB-CHZU-0265.int.audatex.com>
2016-03-03 16:40:57 +0100

    Second checkin

--- SampleJsonWebService.js
+++ SampleJsonWebService.js
@@ -1,6 +1,19 @@
 var http = require('http');
 var fs = require('fs');
 
+function saveFile() {
+	var fs = require("fs");
+
+	console.log("Going to write into existing file");
+	var updateTime = new Date();
+	fs.writeFile('save.dat', '{ "map" : "general", "position": "10,25", "updateTime" : "' + updateTime + '" }',  function(err) {
+	if (err) {
+		return console.error(err);
+	}
+	console.log("Data written successfully!");
+});
+}
+
 http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
   console.log(request.url);
@@ -8,6 +21,11 @@ http.createServer(function (request, response) {
   if(request.url == "/about") {
 	response.write('{"author": "Michael Sundgaard", "company" : "Opus Magus"}');
 	response.end();
+  }
+  if(request.url == "/save") {	
+		saveFile();
+		response.write('{ "status": "success"}');
+		response.end();
   }
   else {
 	fs.readFile('save.dat', (err, data) => {		
