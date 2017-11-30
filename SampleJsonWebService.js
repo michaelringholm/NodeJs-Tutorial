150afd5048465a0d1862c66ba4a22c41c0a68736
Michael Ringholm Sundgaard <michaelringholm@gmail.com>
2017-11-30 21:59:28 +0100

    Added app.yaml for GCP

--- SampleJsonWebService.js
+++ SampleJsonWebService.js
@@ -51,9 +51,9 @@ http.createServer(function (request, response) {
 		response.end();
 	});
   }
-}).listen(1337, "127.0.0.1");
+}).listen(8080, "127.0.0.1");
 
-console.log('Server running at http://127.0.0.1:1337/');
+console.log('Server running at http://127.0.0.1:8080/');
 
 /*
 if (req.method == 'POST') {
