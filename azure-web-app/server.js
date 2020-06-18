bd3ae83931e8dbe8fb418ad272b39c9b89105c44
Michael Ringholm Sundgaard <michaelringholm@gmail.com>
2020-06-18 22:32:43 +0200

    Refactored codeW

--- azure-web-app/server.js
+++ azure-web-app/server.js
@@ -11,61 +11,33 @@ const QRCode = require(`qrcode`)
 var port = process.env.PORT || 8080;
 var fileUploadInProgress = false;
 var validToken = "";
+var azBlobStoreFacade = new AZBlobStoreFacade();
+var htmlFacade = new HTMLFacade();
 
-async function beginFileUpload(request, response, callback) {
-  fileUploadInProgress = true;
+function AZBlobStoreFacade() {
+  var _this = this;
+  var AZURE_STORAGE_CONNECTION_STRING;
 
-  var form = new formidable.IncomingForm();
-  form.maxFileSize = 8*1024*1024*1024;
-  form.parse(request, async function(err, fields, files) {
-    console.log("parsing form...");
-    for(var fileIndex in files) {
-      var file = files[fileIndex];
-      await uploadBlobAsync(file.path, file.name);
-    }
-    if(callback) callback(response, file.path);
-  });
+  this.downloadBlobAsync = async function() {
 
+  };
 
-  //const FORM_URLENCODED = 'application/x-www-form-urlencoded';
-  
-  /*const FORM_MULTIPART = 'multipart';
-  if(request.headers['content-type'].startsWith(FORM_MULTIPART)) {
-      var localFilePath = "temp/" + uuidv1();
-      //var stream = fs.createWriteStream(localFilePath);
-      request.on('data', chunk => {
-        fs.appendFileSync(localFilePath, chunk);
-          //stream.write(chunk);
-          //body += chunk.toString();
-      });
-      request.on('end', async() => {          
-          //await uploadBlobAsync(localFilePath);
-          console.log("file written " + localFilePath);
-          if(callback) callback(response, localFilePath);
-      });
-  }
-  else if(callback) callback();  */
-}
+  this.beginFileUpload = async function (request, response, callback) {
+    fileUploadInProgress = true;
 
-function getPayloadData(request, response, callback) {  
-  if(request.headers['content-type'].startsWith("application/json")) {
-    var jsonPayload = "";
-    request.on('data', chunk => {      
-      jsonPayload += chunk.toString();
+    var form = new formidable.IncomingForm();
+    form.maxFileSize = 8*1024*1024*1024;
+    form.parse(request, async function(err, fields, files) {
+      console.log("parsing form...");
+      for(var fileIndex in files) {
+        var file = files[fileIndex];
+        await _this.uploadBlobAsync(file.path, file.name);
+      }
+      if(callback) callback(response, file.path);
     });
-    request.on('end', async() => {
-        var payload = JSON.parse(jsonPayload);
-        console.log("jsonPayload="+jsonPayload);
-        if(callback) callback(response, payload);
-    });
-  }
-}
+  };
 
-async function uploadBlobAsync(localFilePath, originalFileName) {    
-    // Create a unique name for the blob
-    var AZURE_STORAGE_CONNECTION_STRING = process.env.AZURE_STORAGE_CONNECTION_STRING;
-    AZURE_STORAGE_CONNECTION_STRING=process.env["AZURE_STORAGE_CONNECTION_STRING"];
-    console.log("AZURE_STORAGE_CONNECTION_STRING="+AZURE_STORAGE_CONNECTION_STRING);
+  this.uploadBlobAsync = async function (localFilePath, originalFileName) {    
     // Create the BlobServiceClient object which will be used to create a container client
     const blobServiceClient = BlobServiceClient.fromConnectionString(AZURE_STORAGE_CONNECTION_STRING);
 
@@ -79,7 +51,7 @@ async function uploadBlobAsync(localFilePath, originalFileName) {
     var blobName = originalFileName; //uuidv1();
     var blockBlobClient = containerClient.getBlockBlobClient(blobName);
 
-    console.log('\nUploading to Azure storage as blob:\n\t', blobName);
+    console.log('Uploading blob to Azure storage with name: ', blobName);
 
     // Upload data to the blob
     //var data = 'Hello, World!';
@@ -88,55 +60,90 @@ async function uploadBlobAsync(localFilePath, originalFileName) {
     //var uploadBlobResponse = await blockBlobClient.uploadBlobResponse(data, data.length);
     console.log("Blob was uploaded successfully. requestId: ", uploadBlobResponse.requestId);
     fileUploadInProgress = false;
+  };
+
+  this.downloadBlobAsync = async function (localFilePath, blobName) {    
+    const blobServiceClient = BlobServiceClient.fromConnectionString(AZURE_STORAGE_CONNECTION_STRING);
+    var containerName = "backup";
+    var containerClient = blobServiceClient.getContainerClient(containerName);
+    var blockBlobClient = containerClient.getBlockBlobClient(blobName);
+    console.log('Downloading blob from Azure storage with name: ', blobName);
+    var uploadBlobResponse = await blockBlobClient.download(localFilePath);
+    console.log("Blob " + blobName + "was downloaded successfully.", uploadBlobResponse.requestId);
+  };  
+
+  this.getBlobListAsync = async function() {    
+    // Create a unique name for the blob
+    var AZURE_STORAGE_CONNECTION_STRING = process.env.AZURE_STORAGE_CONNECTION_STRING;
+    AZURE_STORAGE_CONNECTION_STRING=process.env["AZURE_STORAGE_CONNECTION_STRING"];
+    console.log("AZURE_STORAGE_CONNECTION_STRING="+AZURE_STORAGE_CONNECTION_STRING);
+    // Create the BlobServiceClient object which will be used to create a container client
+    const blobServiceClient = BlobServiceClient.fromConnectionString(AZURE_STORAGE_CONNECTION_STRING);
+
+    // Create a unique name for the container
+    //const containerName = 'quickstart' + uuidv1();
+    var containerName = "backup";
+
+    console.log('\nCreating container...');
+    console.log('\t', containerName);
+
+    var containerClient = blobServiceClient.getContainerClient(containerName);
+    var blobList = new Array();
+    for await (const blob of containerClient.listBlobsFlat()) {
+      console.log("Blob ${i++}: ${blob.name}");
+      blobList.push(blob);
+    }
+    console.log('\nBlob List:\n\t', blobList);
+    return blobList;
+  };
+
+  var construct = function() {
+    AZURE_STORAGE_CONNECTION_STRING = process.env.AZURE_STORAGE_CONNECTION_STRING;
+    AZURE_STORAGE_CONNECTION_STRING=process.env["AZURE_STORAGE_CONNECTION_STRING"];
+    console.log("AZURE_STORAGE_CONNECTION_STRING="+AZURE_STORAGE_CONNECTION_STRING);
+  };
+
+  construct();
 }
 
-async function getBlobListAsync() {    
-  // Create a unique name for the blob
-  var AZURE_STORAGE_CONNECTION_STRING = process.env.AZURE_STORAGE_CONNECTION_STRING;
-  AZURE_STORAGE_CONNECTION_STRING=process.env["AZURE_STORAGE_CONNECTION_STRING"];
-  console.log("AZURE_STORAGE_CONNECTION_STRING="+AZURE_STORAGE_CONNECTION_STRING);
-  // Create the BlobServiceClient object which will be used to create a container client
-  const blobServiceClient = BlobServiceClient.fromConnectionString(AZURE_STORAGE_CONNECTION_STRING);
+function HTMLFacade() {
+  var _this = this;
 
-  // Create a unique name for the container
-  //const containerName = 'quickstart' + uuidv1();
-  var containerName = "backup";
-
-  console.log('\nCreating container...');
-  console.log('\t', containerName);
-
-  var containerClient = blobServiceClient.getContainerClient(containerName);
-  var blobList = new Array();
-  for await (const blob of containerClient.listBlobsFlat()) {
-    console.log("Blob ${i++}: ${blob.name}");
-    blobList.push(blob);
-  }
-  console.log('\nBlob List:\n\t', blobList);
-  return blobList;
-}
-
-function setHtmlValue(html, tag, value) {
+  this.setHtmlValue = function(html, tag, value) {
     if(value == undefined) return html;
     html = html.toString().replace(new RegExp(tag, 'g'), value);
     return html;
-}
+  };
 
-function parseHtml(html, model) {
+  this.getCookieValue = function(cookie, key) {
+    if(cookie == undefined || cookie.length == 0)
+      return null;
+    const COOKIE_AUTH_KEY="Authorization=";
+    var tokenStart = cookie.indexOf(COOKIE_AUTH_KEY);
+    var token = cookie.substring(tokenStart+COOKIE_AUTH_KEY.length);
+    var tokenEnd = token.indexOf(";");
+    if(tokenEnd > 0)
+      token = token.substring(0, tokenEnd);
+    token = token.replace("Bearer", "").trim();
+    return token;
+  };
+
+  this.parseHtml = function(html, model) {
     var parsedHtml = html;
     for(var key in model) {
       console.log("key="+key);
       console.log("value=" +model[key]);
     }
-    //parsedHtml = setHtmlValue(parsedHtml, "{fileUploadInProgress}", fileUploadInProgress);
-    /*parsedHtml = setHtmlValue(parsedHtml, "{squidSubStatus}", squidSubStatus);
-    parsedHtml = setHtmlValue(parsedHtml, "{squidStatus}", squidStatus);
-    parsedHtml = setHtmlValue(parsedHtml, "{openPortsLog}", openPortsLog);
-    parsedHtml = setHtmlValue(parsedHtml, "{cpuLog}", cpuLog);
-    parsedHtml = setHtmlValue(parsedHtml, "{url}", url);*/
+    //parsedHtml = _this.setHtmlValue(parsedHtml, "{fileUploadInProgress}", fileUploadInProgress);
+    /*parsedHtml = _this.setHtmlValue(parsedHtml, "{squidSubStatus}", squidSubStatus);
+    parsedHtml = _this.setHtmlValue(parsedHtml, "{squidStatus}", squidStatus);
+    parsedHtml = _this.setHtmlValue(parsedHtml, "{openPortsLog}", openPortsLog);
+    parsedHtml = _this.setHtmlValue(parsedHtml, "{cpuLog}", cpuLog);
+    parsedHtml = _this.setHtmlValue(parsedHtml, "{url}", url);*/
     return parsedHtml;
-  }
+  };
   
-  function writeCss(response, cssPath) {
+  this.writeCss = function(response, cssPath) {
     response.writeHead(200, { 'Content-Type': 'text/css' });
     fs.readFile('./static/' + cssPath, null, function (error, css) {
       if (error) {
@@ -148,9 +155,9 @@ function parseHtml(html, model) {
       }
       response.end();      
     });
-  }
+  };
 
-  function writeJs(response, jsPath) {
+  this.writeJs = function(response, jsPath) {
     response.writeHead(200, { 'Content-Type': 'text/javascript' });
     fs.readFile('./static/' + jsPath, null, function (error, js) {
       if (error) {
@@ -162,9 +169,20 @@ function parseHtml(html, model) {
       }
       response.end();      
     });
-  }  
+  };
+
+  this.writeJson = function(response, statusCode, responseData, bearerToken) {
+    if(bearerToken)
+      response.writeHead(statusCode, { 'Content-Type': 'application/json', "Authorization": bearerToken });
+    else
+      response.writeHead(statusCode, { 'Content-Type': 'application/json' });
+    
+    var jsonResponse = JSON.stringify(responseData);
+    response.write(jsonResponse);
+    response.end();
+  };  
   
-  function writeHtmlPage(response, pagePath, model, bearerToken) {
+  this.writeHtmlPage = function(response, pagePath, model, bearerToken) {
     if(bearerToken)
       response.writeHead(200, { 'Content-Type': 'text/html', "Authorization": bearerToken });
     else
@@ -175,25 +193,30 @@ function parseHtml(html, model) {
         response.writeHead(404);
         response.write('file not found');
       } else {
-        response.write(parseHtml(html, model));
+        response.write(_this.parseHtml(html, model));
       }
       response.end();
     });
-  }
+  };
 
-function writeJson(response, statusCode, responseData, bearerToken) {
-  if(bearerToken)
-    response.writeHead(statusCode, { 'Content-Type': 'application/json', "Authorization": bearerToken });
-  else
-    response.writeHead(statusCode, { 'Content-Type': 'application/json' });
-  
-  var jsonResponse = JSON.stringify(responseData);
-  response.write(jsonResponse);
-  response.end();
+  this.getRequestData = function(request, response, callback) {  
+    if(request.headers['content-type'].startsWith("application/json")) {
+      var jsonPayload = "";
+      request.on('data', chunk => {      
+        jsonPayload += chunk.toString();
+      });
+      request.on('end', async() => {
+          var payload = JSON.parse(jsonPayload);
+          console.log("jsonPayload="+jsonPayload);
+          if(callback) callback(response, payload);
+      });
+    }
+  };  
+
 }
 
 function uploadComplete(response, localFilePath) {
-  //writeHtmlPage(response, './static/html/index.html');
+  //htmlFacade.writeHtmlPage(response, './static/html/index.html');
   writeJson(response, 200, { "message" : "Upload completed." }, "Bearer " + validToken);
 }
 
@@ -210,71 +233,60 @@ function login(response, payload) {
   writeJson(response, 401, { "Message": "Invalid login or password" });
 }
 
-function getCookieValue(cookie, key) {
-  if(cookie == undefined || cookie.length == 0)
-    return null;
-  const COOKIE_AUTH_KEY="Authorization=";
-  var tokenStart = cookie.indexOf(COOKIE_AUTH_KEY);
-  var token = cookie.substring(tokenStart+COOKIE_AUTH_KEY.length);
-  var tokenEnd = token.indexOf(";");
-  if(tokenEnd > 0)
-    token = token.substring(0, tokenEnd);
-  token = token.replace("Bearer", "").trim();
-  return token;
-}
+async function processRequest(request, response) {
+  console.log("function triggered by URL " + request.url);
+  var authenticatorKey = authenticator.generateKey();
+  var qrCodeUri = authenticator.generateTotpUri(authenticatorKey, "iHedge Blob Uploader", "iHedge", "SHA1", 6, 30);
+  QRCode.toDataURL(qrCodeUri, function (error, image) { 
+    //console.log(image);
+  });
+  var model = {};
 
-var server = http.createServer(async function (request, response) {
-    console.log("function triggered by URL " + request.url);
-    var authenticatorKey = authenticator.generateKey();
-    var qrCodeUri = authenticator.generateTotpUri(authenticatorKey, "iHedge Blob Uploader", "iHedge", "SHA1", 6, 30);
-    QRCode.toDataURL(qrCodeUri, function (error, image) { 
-      //console.log(image);
-    });
-    var model = {};
-
-    if(request.url.startsWith("/css/")) writeCss(response, request.url);
-    else if(request.url.startsWith("/js/")) writeJs(response, request.url);
-    else if(request.url.startsWith("/login")) {
-      getPayloadData(request, response, login);      
+  if(request.url.startsWith("/css/")) htmlFacade.writeCss(response, request.url);
+  else if(request.url.startsWith("/js/")) writeJs(response, request.url);
+  else if(request.url.startsWith("/login")) {
+    getRequestData(request, response, login);      
+  }
+  else {
+    // ***** Authorized section ******
+    var token = request.headers.authorization;
+    if(token == undefined)
+      token = htmlFacade.getCookieValue(request.headers.cookie, "Authorization");
+    if(token == undefined) {
+      console.log("No token found in headers!");
+      htmlFacade.writeHtmlPage(response, './static/html/login.html', model);
+      return;
     }
     else {
-      // ***** Authorized section ******
-      var token = request.headers.authorization;
-      if(token == undefined)
-        token = getCookieValue(request.headers.cookie, "Authorization");
-      if(token == undefined) {
-        console.log("No token found in headers!");
-        writeHtmlPage(response, './static/html/login.html', model);
+      if(token != validToken) {
+        console.log("Token invalid or expired!");
+        htmlFacade.writeHtmlPage(response, './static/html/login.html', model);
         return;
       }
-      else {
-        if(token != validToken) {
-          console.log("Token invalid or expired!");
-          writeHtmlPage(response, './static/html/login.html', model);
-          return;
-        }
-        // When authorized check the wanted URL    
-      if(request.url.startsWith("/upload")) {
-        await beginFileUpload(request, response, uploadComplete);
-      }
+      
+      // AUTHORIZATION SUCCESSFUL: Process the wanted URL    
+      if(request.url.startsWith("/upload"))
+        await azBlobStoreFacade.beginFileUpload(request, response, uploadComplete);
+      else if(request.url.startsWith("/download-file"))
+        await azBlobStoreFacade.beginDownload(request, response, uploadComplete);        
       else if(request.url.startsWith("/list-files")) {
-        var blobList = await getBlobListAsync();
+        var blobList = await azBlobStoreFacade.getBlobListAsync();
         writeJson(response, 200, { "message" : "Here is the blob list...", "blobList": blobList }, "Bearer " + validToken);
       }
-      else if(request.url.startsWith("/index")) writeHtmlPage(response, './static/html/index.html');
-      else writeHtmlPage(response, './static/html/index.html');
+      else if(request.url.startsWith("/index")) htmlFacade.writeHtmlPage(response, './static/html/index.html');
+      else htmlFacade.writeHtmlPage(response, './static/html/index.html');
     }
   }
-    
-    /*res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write("<form method=\"post\" action=\"/upload\" enctype=\"multipart/form-data\">");
-    res.write("<input type=\"file\" id=\"fileToUpload\"></input>");
-    res.write("<input type=\"submit\" value=\"Upload\"></input>");
-    res.write("</form>");
-    res.end('iHedge Blob Uploader');*/
-    
-});
+};
+  
+  /*res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write("<form method=\"post\" action=\"/upload\" enctype=\"multipart/form-data\">");
+  res.write("<input type=\"file\" id=\"fileToUpload\"></input>");
+  res.write("<input type=\"submit\" value=\"Upload\"></input>");
+  res.write("</form>");
+  res.end('iHedge Blob Uploader');*/
 
+var server = http.createServer(processRequest);
 server.timeout = 600*1000;
 server.listen(port);
 
--- azure-web-app/static/js/upload.js
+++ azure-web-app/static/js/upload.js
@@ -2,25 +2,13 @@ $(function() {
   console.log("jquery init called!");
   const azureBlobStoreClient = new AzureBlobStoreClient();
   azureBlobStoreClient.listFiles();
-  //$("div#rrr").dropzone({ url: "/upload" });
-  //wireupFileUploadEvent();
 
-  //Dropzone.autoDiscover = false;
-
-  /*$('#someDiv').dropzone({
-      url: "upload",
-      init: function () {
-            this.on("success", function (file, response) {
-                alert(response); // or alert(file.xhr.response);
-            });
-        }
-  });*/
-
-  $("#fileUploadDropZone").get(0).dropzone.options.maxFilesize = 8*1014;
-  $("#fileUploadDropZone").get(0).dropzone.options.timeout = 600 * 1000;
+  $("#fileUploadDropZone").get(0).dropzone.options.maxFilesize = 8*1024; // 8GB
+  $("#fileUploadDropZone").get(0).dropzone.options.timeout = 600 * 1000; // 10 minutes
   $("#fileUploadDropZone").get(0).dropzone.on("complete", function(file, xhr, formData) {
     azureBlobStoreClient.listFiles();
   });
+  $(".blobItem").click(azureBlobStoreClient.downloadFile);
 });
 
 function AzureBlobStoreClient() {
@@ -57,33 +45,36 @@ function AzureBlobStoreClient() {
       contentType: "application/json"
     });    
   };
+
+  this.downloadFile = function() {
+    console.log("Downloading file from Azure blob store...");
+    var data = { fileName = $(this) };
+    $.ajax({
+      type: "POST",
+      url: "download-file",
+      data: JSON.stringify(data),
+      success: function(response, status, xhr) {
+        var contentType = xhr.getResponseHeader("Content-Type");
+        var authorization = xhr.getResponseHeader("Authorization");
+        document.cookie = "Authorization=" + authorization + ";" 
+        /*$("#blobList").empty();
+        for(var blobListIndex in response.blobList) {
+          var blobItem = response.blobList[blobListIndex];
+          var newBlobItem = $("#blobItemTemplate").clone();
+          $(newBlobItem).removeAttr("id");
+          $(newBlobItem).removeClass("template");
+          $(newBlobItem).find(".name").html(blobItem.name);
+          $(newBlobItem).find(".createdDate").html(blobItem.properties.createdOn);
+          $(newBlobItem).find(".contentLength").html(blobItem.properties.contentLength);
+          $("#blobList").append(newBlobItem);
+        }*/
+        console.log("Done downloading file from Azure blob store.");
+      },
+      error: function(xhr, status, reason) {
+        console.log("Downloading file from Azure blob store failed!");
+      },
+      //dataType: "application/json",
+      contentType: "application/json"
+    });    
+  };  
 }
-
-/*function wireupFileUploadEvent() {
-  console.log("Uploading file...");
-  const fileInput = document.getElementById("inputFile");
-
-  const uploadFileFunc = file => {    
-    const API_ENDPOINT = "upload";
-    const request = new XMLHttpRequest();
-    const formData = new FormData();
-
-    $("#uploadStatus").html("Upload started...");
-    $("#uploadStatusBlock").show();
-    request.open("POST", API_ENDPOINT, true);
-    request.onreadystatechange = () => {
-      if (request.readyState === 4 && request.status === 200) {
-        $("#uploadStatus").html("Upload complete.");
-        $("#uploadStatusBlock").hide();
-        $("#inputFile").val("");
-      }
-    };
-    formData.append("file", file);
-    request.send(formData);
-  };
-
-  fileInput.addEventListener("change", event => {
-    const files = event.target.files;
-    uploadFileFunc(files[0]);
-  }); 
-}*/
